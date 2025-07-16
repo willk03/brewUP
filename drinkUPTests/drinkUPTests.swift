97c6fbb4ee05bb7bb506bf7706844c0f294414a9
Will Kuster <will.kuster6@gmail.com>
2025-07-16 19:15:34 -0400

    Initial Commit

---
+++ drinkUPTests/drinkUPTests.swift
@@ -0,0 +1,17 @@
+//
+//  drinkUPTests.swift
+//  drinkUPTests
+//
+//  Created by Will Kuster on 7/16/25.
+//
+
+import Testing
+@testable import drinkUP
+
+struct drinkUPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
